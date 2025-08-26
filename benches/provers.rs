e8240bc0c0b3b94f231f7587fcf3f964d98c0152
Andrew Zitek-Estrada <1497456+z-tech@users.noreply.github.com>
2025-08-26 14:40:27 +0200

    TimeProver and TimeProductProver parallelization w/ Rayon (#80)
    * rayon_parallelize
    
    * lint
    
    * TimeProductProver multithreaded chkpt
    
    * cleanup bench

---
+++ benches/provers.rs
@@ -0,0 +1,74 @@
+use ark_std::{hint::black_box, time::Duration};
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, BatchSize, BenchmarkGroup, Criterion,
+};
+
+use space_efficient_sumcheck::{
+    multilinear::TimeProver,
+    multilinear_product::TimeProductProver,
+    prover::{ProductProverConfig, Prover, ProverConfig},
+    streams::multivariate_product_claim,
+    tests::{BenchStream, F128},
+    ProductSumcheck, Sumcheck,
+};
+
+fn get_bench_group(c: &mut Criterion) -> BenchmarkGroup<'_, WallTime> {
+    let mut group = c.benchmark_group("sumcheck_prover");
+    group
+        .sample_size(10)
+        .warm_up_time(Duration::from_secs(2))
+        .measurement_time(Duration::from_secs(10));
+    group
+}
+
+fn time_prover_bench(c: &mut Criterion) {
+    let num_vars = 24usize;
+    get_bench_group(c).bench_function("time_prover", |bencher| {
+        bencher.iter_batched(
+            || {
+                let stream = BenchStream::<F128>::new(num_vars);
+                TimeProver::<F128, BenchStream<F128>>::new(
+                    <TimeProver<F128, BenchStream<F128>> as Prover<F128>>::ProverConfig::default(
+                        stream.claimed_sum,
+                        num_vars,
+                        stream,
+                    ),
+                )
+            },
+            |mut prover: TimeProver<F128, BenchStream<F128>>| {
+                black_box(Sumcheck::<F128>::prove::<
+                    BenchStream<F128>,
+                    TimeProver<F128, BenchStream<F128>>,
+                >(&mut prover, &mut ark_std::test_rng()));
+            },
+            BatchSize::LargeInput,
+        )
+    });
+}
+
+fn time_product_prover_bench(c: &mut Criterion) {
+    let num_vars = 24usize;
+    get_bench_group(c).bench_function("time_product_prover", |bencher| {
+        bencher.iter_batched(
+            || {
+                let stream = BenchStream::<F128>::new(num_vars);
+                let streams: Vec<BenchStream<F128>> = vec![stream.clone(), stream.clone()];
+                TimeProductProver::<F128, BenchStream<F128>>::new(ProductProverConfig::default(
+                    multivariate_product_claim(streams.clone()),
+                    num_vars,
+                    streams,
+                ))
+            },
+            |mut prover: TimeProductProver<F128, BenchStream<F128>>| {
+                black_box(ProductSumcheck::<F128>::prove::<
+                    BenchStream<F128>,
+                    TimeProductProver<F128, BenchStream<F128>>,
+                >(&mut prover, &mut ark_std::test_rng()));
+            },
+            BatchSize::LargeInput,
+        )
+    });
+}
+
+criterion_group!(benches, time_product_prover_bench, time_prover_bench);
+criterion_main!(benches);
