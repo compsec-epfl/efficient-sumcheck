001702b49f3bcf4234ad824fa4b3d2911bf8ae93
Andrew Z <1497456+z-tech@users.noreply.github.com>
2025-11-07 22:16:17 +0100

    fix no default features

--- src/multilinear/provers/time/reductions/tablewise.rs
+++ src/multilinear/provers/time/reductions/tablewise.rs
@@ -1,15 +1,10 @@
 use ark_ff::Field;
-#[cfg(feature = "parallel")]
 use ark_std::{cfg_chunks, vec::Vec};
 #[cfg(feature = "parallel")]
-use rayon::{
-    iter::{IntoParallelIterator, ParallelIterator},
-    prelude::ParallelSlice,
-};
+use rayon::{iter::ParallelIterator, prelude::ParallelSlice};
 
 pub fn reduce_evaluations<F: Field>(src: &mut Vec<Vec<F>>, verifier_message: F) {
     let out: Vec<Vec<F>> = cfg_chunks!(src, 2)
-        .into_par_iter()
         .map(|chunk| {
             chunk[0]
                 .iter()
